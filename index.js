7bb16083379f338ef79d608d71eadfb9a91f9562
leroyjen <46627866+leroyjen@users.noreply.github.com>
2019-01-12 10:49:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Jimbo it works!");
+    response.end("Come on Dock me for fucks sake!");
 
 });
 
